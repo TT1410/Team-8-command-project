a62722c38ffc817858738ee8d50d99a4835dccc9
SSP0d <spodkopay@gmail.com>
2022-11-13 19:30:09 +0200

    ADDRESS_BOOK рефакторинг
    ADDRESS_BOOK рефакторинг в handlers

--- services/handlers/birthday.py
+++ services/handlers/birthday.py
@@ -2,7 +2,7 @@ from datetime import date, timedelta
 from typing import Optional
 
 from services.decorators import input_error, route
-from services.utils import ADDRESS_BOOK
+from services.utils import AddressBook
 
 
 @route("change-bd")
@@ -13,7 +13,7 @@ def change_contact_birthday(name: str) -> str:
     Пользователь вводит команду change-bd и имя обязательно через пробел.
     Пример команды: change-bd UserName
     """
-    contact = ADDRESS_BOOK()[name]
+    contact = AddressBook()[name]
 
     birthday = input(
         f"Enter the date of birth of the contact '{name}' in the format YYYY.MM.DD or DD.MM.YYYY: ")
@@ -21,7 +21,7 @@ def change_contact_birthday(name: str) -> str:
     contact.change_birthday(birthday)
 
     # Temporary command due to impossibility to change object in the file
-    ADDRESS_BOOK().change_contact(contact)
+    AddressBook().change_contact(contact)
 
     return f"\nDate of birth {contact.birthday.value} of the contact '{name}' successfully saved"
 
@@ -42,7 +42,7 @@ def search_birthday_boy(days: str) -> Optional[str]:
     format_contacts = ""
     target_date = date.today() + timedelta(days=days)
 
-    for contact in ADDRESS_BOOK().get_all_contacts():
+    for contact in AddressBook().get_all_contacts():
         if not contact.birthday:
             continue
 
@@ -63,7 +63,7 @@ def days_before_birthday(name: str) -> str:
     Пользователь вводит команду days-bd и имя контакта, обязательно через пробел.
     Пример команды: days-bd UserName
     """
-    days = ADDRESS_BOOK()[name].days_to_birthday()
+    days = AddressBook()[name].days_to_birthday()
 
     return (f"Until the birthday of {name} {days} days" if days else
             f"Contact '{name}' does not have a birthday recorded. "
