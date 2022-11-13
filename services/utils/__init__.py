2e8e5f249ee7fbf3fceccfc6051d8952831ac3f6
TT1410 <112493080+TT1410@users.noreply.github.com>
2022-11-13 18:53:29 +0200

    fix bag

---
+++ services/utils/__init__.py
@@ -0,0 +1,13 @@
+from .address_book import AddressBook
+from .notes import Notes
+from .register_handlers import register_message_handler, ROUTE_MAP
+from .input_parser import text_parsing
+
+
+__all__ = (
+    "AddressBook",
+    "Notes",
+    "register_message_handler",
+    "ROUTE_MAP",
+    "text_parsing"
+)
---
+++ services/utils/address_book/__init__.py
@@ -0,0 +1,14 @@
+from .address_book import AddressBook
+from .record import Record
+from .name import Name
+from .phone import Phone
+
+ADDRESS_BOOK = AddressBook
+
+__all__ = (
+    "AddressBook",
+    "Record",
+    "Name",
+    "Phone",
+    "ADDRESS_BOOK",
+)
