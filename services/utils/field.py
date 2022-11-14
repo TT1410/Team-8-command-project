07a56b159085cadf48cd3ce46d20b75c56e88caa
SSP0d <spodkopay@gmail.com>
2022-11-14 21:50:00 +0200

    field fix
    поправлен калcс Field

--- services/utils/field.py
+++ services/utils/field.py
@@ -1,15 +1,15 @@
 
 class Field:
     def __init__(self, value: str):
-        self._value = value
+        self.value = value
 
     @property
     def value(self):
-        return self._value
+        return self.value
 
     @value.setter
     def value(self, value):
-        self._value = value
+        self.value = value
 
     def __repr__(self):
         return f"{self.__class__.__name__}(value={self.value!r})"
