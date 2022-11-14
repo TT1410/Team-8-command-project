a05e2d95ac23398af5e9c881db6b17785f5b6813
SSP0d <spodkopay@gmail.com>
2022-11-14 22:13:25 +0200

    Update field.py

--- services/utils/field.py
+++ services/utils/field.py
@@ -1,15 +1,16 @@
 
 class Field:
     def __init__(self, value: str):
+        self._value = None
         self.value = value
 
     @property
     def value(self):
-        return self.value
+        return self._value
 
     @value.setter
     def value(self, value):
-        self.value = value
+        self.value = _value
 
     def __repr__(self):
         return f"{self.__class__.__name__}(value={self.value!r})"
